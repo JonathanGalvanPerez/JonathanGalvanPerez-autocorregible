4ff75627acda741a641a9e829f555ef0f00c623e
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-06-26 03:04:19 -0300

    store token in localstorage

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from '../features/login/loginSlice';
 
+const preloadedState = {
+  login: {
+    token: localStorage.getItem('token')
+  },
+};
+
 export const store = configureStore({
   reducer: {
     login: loginReducer
   },
+  preloadedState
 });
+
+window.onbeforeunload = () => {
+  localStorage.setItem('token', store.getState().login.token);
+}
