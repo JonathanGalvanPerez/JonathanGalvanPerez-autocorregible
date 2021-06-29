3c798f396b9894d3f7b97e887fb52bb891eaa143
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-06-29 05:09:32 -0300

    add protected routes

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -2,6 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import MainPage from '../pages/MainPage';
 import LoginPage from './../pages/LoginPage';
+import { useSelector } from 'react-redux';
+import { isLoggedIn } from '../features/login/loginSlice';
+
+const ProtectedRoute = (props) => {
+    const _isLoggedIn = useSelector(isLoggedIn);
+    if(!_isLoggedIn)
+        return (
+            <Redirect to="/login" />
+        )
+    return (
+        <Route {...props} />
+    )
+}
 
 export default function AppRoutes() {
     return (
@@ -9,7 +22,7 @@ export default function AppRoutes() {
             <Router>
                 <Switch>
                     <Route exact path="/login" component={LoginPage} />
-                    <Route path="/" component={MainPage} />
+                    <ProtectedRoute path="/" component={MainPage} />
                     <Redirect path="/**" to="/" />
                 </Switch>
             </Router>
