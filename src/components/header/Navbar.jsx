701a12548af8e1ac537614ce124754da3ed7a950
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-07-01 20:16:13 -0300

    add navbar

---
+++ src/components/header/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux';
+import { logout } from '../../features/login/loginSlice';
+
+export default function Navbar() {
+    const dispatch = useDispatch();
+    
+    const handleLogoutButton = () => {
+        dispatch(logout())
+        console.log('deslogueado con exito');
+    }
+    return (
+        <ul className="nav nav-tabs bg-primary">
+            <li className="nav-item">
+                <Link className="nav-link" to="/home">Home</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/search">Search</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/login">Login</Link>
+            </li>
+            <button onClick={handleLogoutButton} className="alkemy-btn-danger ml-auto mr-2 mr-sm-3 rounded-pill" >Deslogearse</button>
+        </ul>     
+    )
+}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import MainRoutes from './../routes/MainRoutes';
+import { useSelector } from 'react-redux';
+import { isLoggedIn } from '../features/login/loginSlice';
+import { Redirect } from 'react-router-dom';
+import Navbar from '../components/header/Navbar';
 
 export default function MainPage() {
+    const _isLoggedIn = useSelector(isLoggedIn);
+
+    if(!_isLoggedIn)
+        return (
+            <Redirect to="/login" />
+        )
     return (
-        <div>
-            <MainRoutes />
+        <div className="container p-0">
+            <Navbar />
+            <div className="p-2 px-md-0">
+                <MainRoutes />
+            </div>
         </div>
     )
 }
