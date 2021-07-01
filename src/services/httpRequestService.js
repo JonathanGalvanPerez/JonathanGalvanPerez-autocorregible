d31b971035a680e311bef3a5f3ae0d46f25ae425
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-07-01 16:11:11 -0300

    process response data

--- src/services/httpRequestService.js
+++ src/services/httpRequestService.js
@@ -1,22 +1,46 @@
 import axios from "axios";
 import { HERO_API_URL, LOGIN_API_URL } from "../app/config";
 
-const login = (values) => {
-    return axios.post(LOGIN_API_URL, values);
+const login = async (values) => {
+    const { data } = await axios.post(LOGIN_API_URL, values);
+    return data;
 }
 
-const searchHero = (name) => {
-    return axios.post(HERO_API_URL, name);
+const searchHero = async (query) => {
+    let { data } = await axios.get(`${HERO_API_URL}/search/${query}`);
+    if(!data.error)
+        data = data.results.map(hero => (
+            {
+                id: hero.id,
+                name: hero.name,
+                powerstats: hero.powerstats,
+                image: hero.image
+            }));
+    return data;
 }
 
-const getHero = (id) => {
-    return axios.get(`${HERO_API_URL}/${id}`);
+const getHero = async (id) => {
+    let { data } = await axios.get(`${HERO_API_URL}/${id}`);
+    if(!data.error)
+        data = data.results.map(hero => (
+            {
+                name: hero.name,
+                aliases: hero.biography.aliases,
+                workplace: hero.work.base,
+                height: hero.appearance.height,
+                weight: hero.appearance.weight,
+                eyeColor: hero.appearance["eye-color"],
+                hairColor: hero.appearance["hair-color"],
+                image: hero.image.url
+            }
+        ));
+    return data.results;
 }
 
-const RequestServices = {
+const RequestService = {
     login,
     searchHero,
     getHero
 }
 
-export default RequestServices;
+export default RequestService;
