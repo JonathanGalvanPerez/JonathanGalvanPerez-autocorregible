8cd35b4cdf8755be6e47e67286d54e4518457d3b
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-07-01 15:22:48 -0300

    add proxy url

--- src/app/config.js
+++ src/app/config.js
@@ -1,3 +1,3 @@
 const HERO_ACCESS_TOKEN = "4174620429250377";
 export const LOGIN_API_URL = "http://challenge-react.alkemy.org";
-export const HERO_API_URL = "https://superheroapi.com/api/" + HERO_ACCESS_TOKEN;
+export const HERO_API_URL = "https://thingproxy.freeboard.io/fetch/" + "https://superheroapi.com/api/" + HERO_ACCESS_TOKEN;
