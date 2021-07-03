3271bedd37d0ee86c743ff82c91c7990cbd4840b
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-07-03 03:12:23 -0300

    add background images

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './App.css';
 import AppRoutes from './routes/AppRoutes'
 
+const style = {
+  backgroundImage: "url(/images/comic-background.jpg)",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundAttachment: "fixed",
+  minHeight: "100vh"
+}
+
 function App() {
   return (
-    <div className="App">
+    <div className="App py-md-2" style={style}>
       <AppRoutes />
     </div>
   );
