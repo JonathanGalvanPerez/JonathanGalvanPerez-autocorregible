7bc5e90926ebab1d391ba5bcbedb39cf293bcba1
Jonathan Galvan Perez <jonathangalvanperez@gmail.com>
2021-07-05 07:43:45 -0300

    minor changes

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -11,19 +11,17 @@ export default function Navbar() {
         console.log('deslogueado con exito');
     }
     return (
-        <nav className="d-flex bg-primary pl-sm-3">
+        <nav className="d-flex bg-primary px-2 px-sm-3">
+            <img src="/images/apple-touch-icon.png" alt="logo icon" height="50px" className="rounded-md my-2 mr-2" />
             <ul className="nav nav-tabs bg-primary ">
                 <li className="nav-item">
-                    <Link className="nav-link" to="/home">Home</Link>
+                    <Link className="nav-link" to="/home">Inicio</Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to="/search">Search</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/login">Login</Link>
+                    <Link className="nav-link" to="/search">Buscar</Link>
                 </li>
             </ul>
-            <button onClick={handleLogoutButton} className="alkemy-btn-danger ml-auto mr-2 mr-sm-3 rounded-md my-2" >Deslogearse</button>
+            <button onClick={handleLogoutButton} className="alkemy-btn-danger ml-auto rounded-md my-2" >Deslogearse</button>
         </nav>
     )
 }
